59edfe89cadcd0a93091ad91addbdd01a797a443
faisalahammed044274 <setu2014@gmail.com>
2022-12-16 12:36:35 +0600

    javascript loaded

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files(){
+    wp_enqueue_script('main-university-script',get_theme_file_uri('/build/index.js'),array('jquery'),'1.0', true);
     wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
     wp_enqueue_style('font-awesome','//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css');
     wp_enqueue_style('university_main_styles',
