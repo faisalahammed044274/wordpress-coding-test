aa696978fe57b7f163d8fae4f65a1b29f708fa2b
faisalahammed044274 <setu2014@gmail.com>
2022-12-22 00:20:53 +0600

    Dynamic Blog

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,35 @@
-This is the generic blog template.
+<?php
+get_header();
+?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)">
+  </div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Welcome to our blog!</h1>
+    <div class="page-banner__intro">
+      <p>keep up with our latest news.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+  <?php while (have_posts()) {
+    the_post(); ?>
+
+    <div class="post-item">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
+    <div class="metabox">
+      <p>posted by faisal on 22 Dec 2022 in theme blog.</p>
+    </div>
+    <div class="generic-content">
+      <?php the_excerpt(); ?>
+    </div>
+  <?php } ?>
+
+</div>
+
+<?php
+get_footer();
+?>
