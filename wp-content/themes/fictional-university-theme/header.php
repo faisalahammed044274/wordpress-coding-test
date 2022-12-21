363d29e4f3e3ea86340df3bd24e0ea9dcc3355e5
faisalahammed044274 <setu2014@gmail.com>
2022-12-21 23:50:12 +0600

    Own HTML Menu Setup in WP

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -22,14 +22,14 @@
         <nav class="main-navigation">
           <?php wp_nav_menu(array( 'woocommerce_admin_order_preview_line_item_column_sanitize_key(column>)', 'callback' )); ?>
 
-          <!-- <ul>
-            <li><a href="<?php //echo site_url('/about-us'); 
+          <ul>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class= "current-menu-item"'?>><a href="<?php echo site_url('/about-us'); 
                           ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
             <li><a href="#">Blog</a></li>
-          </ul> -->
+          </ul>
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
