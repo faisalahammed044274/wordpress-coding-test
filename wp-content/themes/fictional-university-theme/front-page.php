4af46ec3e53202dc3a14713fe20802bfb86af541
faisalahammed044274 <setu2014@gmail.com>
2022-12-23 23:53:53 +0600

    Blog Quick Edits & improvements

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -62,7 +62,7 @@
         </div>
       <?php }
       wp_reset_postdata(); ?>
-      <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+      <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
   </div>
 </div>
