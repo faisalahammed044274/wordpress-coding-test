a37def4907cef35def67f0cdd0f229cfe1176691
faisalahammed044274 <setu2014@gmail.com>
2022-12-28 02:30:06 +0600

    Update MU Plugin

--- wp-content/mu-plugins/university_post_types.php
+++ wp-content/mu-plugins/university_post_types.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * Name : Test Plugin
- * Description : This is a event test plugin
- * Version : 1.0.0
- * Author : FaisalAhammed044274
- * 
+ * Name: Test MU Plugin
+ * Description: This is a event test plugin
+ * Version: 1.0.0
+ * Author: FaisalAhammed044274
  */
+
+
 //Add Events
 function university_post_types()
 {
