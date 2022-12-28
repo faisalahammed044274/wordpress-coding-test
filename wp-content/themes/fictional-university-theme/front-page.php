b70164115c7883135e49fddeefc398825f881d22
faisalahammed044274 <setu2014@gmail.com>
2022-12-29 00:41:51 +0600

    Event custom field

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -15,6 +15,29 @@
     <div class="full-width-split__inner">
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
+
+      <?php
+      $homepageEvents = new WP_Query(array(
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'type' => 'numeric'
+          )
+        )
+      ));
+
+
+      while ($homepageEvents->have_posts()) {
+        $homepageEvents->the_post();
+      }
+      ?>
+
       <div class="event-summary">
         <a class="event-summary__date t-center" href="#">
           <span class="event-summary__month">
@@ -26,27 +49,19 @@
           <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
         </a>
         <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-          <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-        </div>
-      </div>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-          <span class="event-summary__month">
-            <?php
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M');
-            ?>
-          </span>
-          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-          <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
+          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+          <p><?php if (has_excerpt()) {
+                echo get_the_excerpt();
+              } else {
+                echo wp_trim_words(get_the_content(), 18);
+              } ?>
+            <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+          </p>
         </div>
       </div>
 
-      <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
     </div>
   </div>
   <div class="full-width-split__two">
